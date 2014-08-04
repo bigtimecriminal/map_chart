ed421a053a4a3d02619e22e767e7cd03502db5e5
Anton K <anton@two-n.com>
2014-08-04 17:34:35 -0400

    US States added as additional layer.

--- makemap.sh
+++ makemap.sh
@@ -4,8 +4,8 @@
 rm subunits.json
 rm subunits.topo.json
 rm merged_subunits.topo.json
-
 rm states.json
+rm mergedStatesAndSubunits.json
 
 exclude=( 'ATA' 'HMD' 'PYF' 'ATF' 'ALA' 'CYM' 'FLK' 'GGY' 'NCL' 'NIU' 'NFK' 'BLM' 'SHN' 'MAF' 'SPM' 'SXM' 'SGS' 'TCA' 'UMI' 'VIR' 'WLF' \
   'NSV' 'FRO' 'ECG' 'EUI' 'SFA' 'FSA' 'SGG' 'ATB' 'ATS' 'PAZ' 'PMD' 'ESC' 'FJI' 'NJM' 'WSM' 'TON' 'NZC' 'KIR' 'NZA' 'GUM' 'MNP' 'PLW' 'REU' 'MUS' \
@@ -15,13 +15,12 @@ query=$(printf "and SU_A3 <> '%s' " "${exclude[@]}")
 query=${query:4}
 echo $query
 
+#construct US states
 ogr2ogr \
   -f GeoJSON \
   -where "adm0_a3='USA'" \
   states.json \
   infiles/ne_10m_admin_1_states_provinces.shp
-
-exit
 
 #construct geoJSON
 ogr2ogr \
@@ -36,12 +35,14 @@ topojson \
   --id-property SOVEREIGNT \
   --simplify-proportion $1\
   --p SOV_A3 \
-  subunits.json
+  subunits.json \
+  states.json
 
-#marge subunits
+#merge subunits
 topojson-merge subunits.topo.json \
   --io=subunits \
   --oo=countries \
+  --key='d.SOV_A3' \
   -o merged_subunits.topo.json
 
 cp merged_subunits.topo.json testbed/data/maps/merged_subunits.topo.json
