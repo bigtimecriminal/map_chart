66191fc2c4a64797cd309ccf32ddd0b3bde9f6ac
Anton K <anton@two-n.com>
2014-08-07 09:55:26 -0400

    Properly creating US second file.

--- makemap.sh
+++ makemap.sh
@@ -14,14 +14,14 @@ exclude=( 'ATA' 'HMD' 'PYF' 'ATF' 'ALA' 'CYM' 'FLK' 'GGY' 'NCL' 'NIU' 'NFK' 'BLM
   'CPV' )
 query=$(printf "and SU_A3 <> '%s' " "${exclude[@]}")
 query=${query:4}
-#query+="and SOV_A3 <> 'US1'"
+query+="and SOV_A3 <> 'US1'"
 
-#construct US states
-#ogr2ogr \
-#  -f GeoJSON \
-#  -where "adm0_a3='USA'" \
-#  states.json \
-#  infiles/ne_10m_admin_1_states_provinces_lakes.shp
+##construct US states
+ogr2ogr \
+  -f GeoJSON \
+  -where "adm0_a3='USA'" \
+  states.json \
+  infiles/ne_10m_admin_1_states_provinces_lakes.shp
 
 #construct geoJSON
 ogr2ogr \
@@ -33,10 +33,18 @@ ogr2ogr \
 #construct topoJSON subunits
 topojson \
   -o subunits.topo.json \
-  --id-property SOV_A3 \
+  --id-property SOVEREIGNT \
   --simplify-proportion $1\
   --p SOV_A3 \
-  subunits.json 
+  subunits.json \
+
+#construct topoJSON states
+topojson \
+  -o states.topo.json \
+  --id-property name \
+  --simplify-proportion $1\
+  --p admin \
+  states.json \
 
 #merge subunits
 topojson-merge subunits.topo.json \
@@ -44,6 +52,18 @@ topojson-merge subunits.topo.json \
   --oo=countries \
   --k='d.properties.SOV_A3' \
   -o merged_subunits.topo.json
+
+#merge states
+topojson-merge states.topo.json \
+  --io=states \
+  --oo=countries2 \
+  --k='d.properties.admin' \
+  -o merged_states.topo.json
+
+topojson -o merged_subunits.topo.json \
+  -- \
+  merged_states.topo.json \
+  merged_subunits.topo.json \
 
 cp merged_subunits.topo.json testbed/data/maps/merged_subunits.topo.json
 
