3e292265dd816cd3ee71286b5b1ecb9a980a36d1
Anton Kulikov <Anton.I.Kulikov@jpl.nasa.gov>
2015-12-22 09:49:30 -0800

    Reverted changes to bring back antarctica.

--- genExcludeFile.sh
+++ genExcludeFile.sh
@@ -2,7 +2,7 @@
 #generates the list of excluded subunits, mostly island groups.
 
 
-exclude=( 'HMD' 'PYF' 'ATF' 'ALA' 'CYM' 'FLK' 'GGY' 'NCL' 'NIU' 'NFK' 'BLM' 'SHN' 'MAF' 'SPM' 'SXM' 'SGS' 'TCA' 'UMI' 'VIR' 'WLF' \
+exclude=( 'ATA' 'ATB' 'HMD' 'PYF' 'ATF' 'ALA' 'CYM' 'FLK' 'GGY' 'NCL' 'NIU' 'NFK' 'BLM' 'SHN' 'MAF' 'SPM' 'SXM' 'SGS' 'TCA' 'UMI' 'VIR' 'WLF' \
   'NSV' 'FRO' 'ECG' 'EUI' 'SFA' 'FSA' 'SGG' 'ATS' 'PAZ' 'PMD' 'ESC' 'FJI' 'NJM' 'WSM' 'TON' 'NZC' 'KIR' 'NZA' 'GUM' 'MNP' 'PLW' 'REU' 'MUS' \
   'BAC' 'CHP' 'ASM' 'SHS' 'SGX' 'FSM' 'SYC' 'ZAI' 'BSI' 'CLP' 'GOI' 'JUI' 'MTQ' 'MYT' 'TEI' \
   'CPV' 'AND' 'DMA' 'COM') #the only sovereign nations excluded are Cabo Verde, Dominica, Andorra and Comoros
