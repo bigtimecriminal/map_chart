66335a9a71c4738ff237555c619187c7339e30e5
Anton Kulikov <Anton.I.Kulikov@jpl.nasa.gov>
2015-12-22 09:46:48 -0800

    Grouped ATB and ATA exclude tags together, to decrease confusion.

--- genExcludeFile.sh
+++ genExcludeFile.sh
@@ -2,8 +2,8 @@
 #generates the list of excluded subunits, mostly island groups.
 
 
-exclude=( 'ATA' 'HMD' 'PYF' 'ATF' 'ALA' 'CYM' 'FLK' 'GGY' 'NCL' 'NIU' 'NFK' 'BLM' 'SHN' 'MAF' 'SPM' 'SXM' 'SGS' 'TCA' 'UMI' 'VIR' 'WLF' \
-  'NSV' 'FRO' 'ECG' 'EUI' 'SFA' 'FSA' 'SGG' 'ATB' 'ATS' 'PAZ' 'PMD' 'ESC' 'FJI' 'NJM' 'WSM' 'TON' 'NZC' 'KIR' 'NZA' 'GUM' 'MNP' 'PLW' 'REU' 'MUS' \
+exclude=( 'HMD' 'PYF' 'ATF' 'ALA' 'CYM' 'FLK' 'GGY' 'NCL' 'NIU' 'NFK' 'BLM' 'SHN' 'MAF' 'SPM' 'SXM' 'SGS' 'TCA' 'UMI' 'VIR' 'WLF' \
+  'NSV' 'FRO' 'ECG' 'EUI' 'SFA' 'FSA' 'SGG' 'ATS' 'PAZ' 'PMD' 'ESC' 'FJI' 'NJM' 'WSM' 'TON' 'NZC' 'KIR' 'NZA' 'GUM' 'MNP' 'PLW' 'REU' 'MUS' \
   'BAC' 'CHP' 'ASM' 'SHS' 'SGX' 'FSM' 'SYC' 'ZAI' 'BSI' 'CLP' 'GOI' 'JUI' 'MTQ' 'MYT' 'TEI' \
   'CPV' 'AND' 'DMA' 'COM') #the only sovereign nations excluded are Cabo Verde, Dominica, Andorra and Comoros
 query=$(printf "and SU_A3 <> '%s' " "${exclude[@]}")
